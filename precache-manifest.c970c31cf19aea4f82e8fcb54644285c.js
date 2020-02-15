7f4576a68c149c4bce81b6015f9a8b27a90bcee4
riunge <61078792+riunge@users.noreply.github.com>
2020-02-15 11:51:37 +0300

    Beta Version, From build

---
+++ precache-manifest.c970c31cf19aea4f82e8fcb54644285c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d5d8ffbaa8ac7fc077081af30708f3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c022b6218e9d2bbc7279",
+    "url": "/static/css/main.3d02e3e0.chunk.css"
+  },
+  {
+    "revision": "2ffdbc59348ab7e0cef2",
+    "url": "/static/js/2.56d977e9.chunk.js"
+  },
+  {
+    "revision": "c022b6218e9d2bbc7279",
+    "url": "/static/js/main.4250519f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
